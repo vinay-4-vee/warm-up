a7cf324e58dcd3ad09117b0d55ce859488539fa4
vinay-4-vee <vinayreddy.taadi@gmail.com>
2025-01-12 21:56:34 +0000

    time check

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
- console.log("Hi Vinay Java script is connected to HTML success");
+console.log("Hi Vinay Java script is connected to HTML success");
+console.log("checking the time taken to update the website");
