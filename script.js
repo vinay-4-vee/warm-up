e8b393b3370d9bf4e439b20b8c588b3a5f983c72
vinay-4-vee <vinayreddy.taadi@gmail.com>
2025-01-31 21:36:43 +0000

    added nav bar with menudrawer for small screens

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+
 let hamburgerIcon =document.getElementById("hamburgerIcon");
 let closeIcon =document.getElementById("closeIcon");
+let menuDrawerContainer=document.getElementById("menuDrawerContainer");
 
-hamburgerIcon.onclick=function(){
+
+    hamburgerIcon.onclick=function(){
     closeIcon.classList.toggle("nav-bar-item-xmark-visibility");
     hamburgerIcon.classList.toggle("nav-bar-item-hamburger-visibility");
+    menuDrawerContainer.classList.toggle("nav-bar-item-xmark-visibility");
+
 };
 
-closeIcon.onclick=function(){
+    closeIcon.onclick=function(){
     closeIcon.classList.toggle("nav-bar-item-xmark-visibility");
     hamburgerIcon.classList.toggle("nav-bar-item-hamburger-visibility");
+    menuDrawerContainer.classList.toggle("nav-bar-item-xmark-visibility");
 };
+});
