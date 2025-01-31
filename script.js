0f7140761cc4a12f6328e1584d3c77130d5e0c24
vinay-4-vee <vinayreddy.taadi@gmail.com>
2025-01-31 13:08:57 +0000

    Hnadling changes for nav bar by adding hamburger icon

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-console.log("Hi Vinay Java script is connected to HTML success");
-console.log("checking the time taken to update the website");
+let hamburgerIcon =document.getElementById("hamburgerIcon");
+let closeIcon =document.getElementById("closeIcon");
+
+hamburgerIcon.onclick=function(){
+    closeIcon.classList.toggle("nav-bar-item-xmark-visibility");
+    hamburgerIcon.classList.toggle("nav-bar-item-hamburger-visibility");
+};
+
+closeIcon.onclick=function(){
+    closeIcon.classList.toggle("nav-bar-item-xmark-visibility");
+    hamburgerIcon.classList.toggle("nav-bar-item-hamburger-visibility");
+};
